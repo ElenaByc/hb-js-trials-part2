776fefca9b9c7137aaa1964aac79a364d77e1ccf
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-04-24 13:22:45 -0700

    feat: complete arrays.js

--- functions.js
+++ functions.js
@@ -2,10 +2,27 @@
 
 // 1. isHometown
 
-// Define your function here
+// def is_hometown(town):
+//     """Given a `town`, return `True` if `town` is 'San Francisco'.
+
+//     Arguments:
+//         town (str): A town
+
+//     Returns:
+//         bool: True if `town` is 'San Francisco'
+//     """
+
+//     return town == "San Francisco"
+function isHometown(town) {
+    return town === 'San Francisco'
+}
+
+
 
 // 2. getFullName
-
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`
+}
 // Define your function here
 
 // 3. calculateTotal

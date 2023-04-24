bd0f9f272f45333cdf4683376ba39877697031c2
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-04-24 13:44:19 -0700

    feat: complete objects.js

--- objects.js
+++ objects.js
@@ -2,10 +2,34 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  let word_counts = {}
+
+  for (const word of phrase.split(" ")) {
+      if (word in word_counts) {
+          word_counts[word] += 1
+      }
+      else word_counts[word] = 1
+  }
+  return word_counts
 }
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+    const melonPrices = {
+        2.50: ['Cantaloupe', 'Honeydew'],
+        2.95: ['Watermelon'],
+        3.25: ['Musk', 'Crenshaw'],
+        14.25: ['Christmas']
+    }
+
+    // if price not in melon_prices:
+    //     return
+
+    //     Check inclusion
+    //     capitals.hasOwnProperty('OR');
+
+    if (!melonPrices.hasOwnProperty(price))
+        return 
+
+    return melonPrices[price].sort()
 }
